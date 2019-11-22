f7ca60a974dea52e24c213e1d150d021d74221e3
akshatdb <akshatd4@gmail.com>
2019-11-22 07:47:11 +0000

    added helmet for security, changed error message and response time

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const express = require('express')
+const express = require('express');
 var geoip = require('geoip-lite');
-const app = express()
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+const app = express();
+const helmet = require('helmet');
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 const requestIp = require('request-ip');
-
+app.use(helmet());
 app.get('/', (req, res) => {
-    res.send(geoip.lookup(requestIp.getClientIp(req))?geoip.lookup(requestIp.getClientIp(req)):{error: 'Unable to fetch location.'});
+    res.setHeader('Content-Type', 'application/json');
+    console.log(requestIp.getClientIp(req));
+    res.send(geoip.lookup(requestIp.getClientIp(req))?geoip.lookup(requestIp.getClientIp(req)):{error: 'Can not fetch location for a private IP'});
 })
 
-app.listen(server_port, server_ip_address, () => console.log(`Example app listening on ${server_ip_address} on port ${server_port}!`))
+app.listen(server_port, server_ip_address, () => console.log(`Example app listening on ${server_ip_address} on port ${server_port}!`));
