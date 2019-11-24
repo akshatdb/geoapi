832fd5297ece20eb6299833c5d54b673576fecf9
akshatd4 <akshatd4@gmail.com>
2019-11-24 21:02:05 +0530

    Added cors

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 const requestIp = require('request-ip');
 app.use(helmet());
+app.use(cors())
 app.get('/', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     console.log(requestIp.getClientIp(req));
